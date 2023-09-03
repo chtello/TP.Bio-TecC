82a3d0252fe08d749b946390bec2aa82bbfd8576
chtelo <chtello@escuelasproa.edu.ar>
2023-09-03 19:18:50 -0300

    base de datos conectada

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-const express = require('express');
+const express = require("express");
 const morgan = require ("morgan");
-const bodyparser = require('bodyparser');
 const path = require ("path");
-const ejs = require('ejs');
+const ejs = require("ejs");
 const mongoose = require ("mongoose");
 const app = express();
-
 module.exports = app;
+
--- server.js
+++ server.js
@@ -1,5 +1,13 @@
+const express = require("express");
 const app = require("./app");
-const port = 3000;
+const mongoose = require("mongoose");
+let port = 3000;
+
+mongoose
+.connect("mongodb+srv://chtello:BaseAdN@cluster0.i9xi2fa.mongodb.net/?retryWrites=true&w=majority")
+    .then(console.log("Connected to database"))
+    .catch((error)=>console.log("tienes un error, tu error es :" + error));
+
 
 app.listen(port, () => {
     console.log(`Servidor escuchando en el puerto:${port}`);
